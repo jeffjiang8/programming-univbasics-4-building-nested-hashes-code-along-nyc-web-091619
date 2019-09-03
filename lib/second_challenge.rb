2304fec26f0ebdc862a8c319771ecd32f612cc9f
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-03 03:00:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,13 +4,13 @@ def second_challenge
       :patriarch{},
       :matriarch{},
       :hero{},
-      :hero_friend{}
+      :hero_friend[]
       }, 
     :capulet => {
       :patriarch{},
       :matriarch{},
       :hero{},
-      :hero_friend{}
+      :hero_friend[]
       }}
 
   
